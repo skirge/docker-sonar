c28cb5706c2a70a91fe8c93f42e006f711f45c6c
Dawid Malinowski <dawidmalina@gmail.com>
2016-03-18 15:43:41 +0100

    Move all props to sonar.properties

--- start-sonar.sh
+++ start-sonar.sh
@@ -13,10 +13,9 @@ if [ "${LDAP}" != "" ]; then
     echo "ldap.group.baseDn=${GROUP_BASE_DN}" >> /opt/app/sonarqube/conf/sonar.properties
 fi
 
-exec java -server -Xms${JVM_MINIMUM_MEMORY:=384m} -Xmx${JVM_MAXIMUM_MEMORY:=768m} ${JAVA_OPTS} -jar /opt/app/sonarqube/lib/sonar-application-$SONAR_VERSION.jar \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  "$@"
+echo "sonar.log.console=${SONARQUBE_LOG_CONSOLE}" >> /opt/app/sonarqube/conf/sonar.properties
+echo "sonar.jdbc.username=${SONARQUBE_JDBC_USERNAME}" >> /opt/app/sonarqube/conf/sonar.properties
+echo "sonar.jdbc.password=${SONARQUBE_JDBC_PASSWORD}" >> /opt/app/sonarqube/conf/sonar.properties
+echo "sonar.jdbc.url=${SONARQUBE_JDBC_URL}" >> /opt/app/sonarqube/conf/sonar.properties
 
+exec java -server -Xms${JVM_MINIMUM_MEMORY:=384m} -Xmx${JVM_MAXIMUM_MEMORY:=768m} ${JAVA_OPTS} -jar /opt/app/sonarqube/lib/sonar-application-$SONAR_VERSION.jar
